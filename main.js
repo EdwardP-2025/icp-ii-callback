11bcce6cfcdf17cd6b9bfd223cd3d475bf1a50de
EdwardP-2025 <edwardp.dev2025@gmail.com>
2025-07-16 11:26:04 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,20 @@ import { AuthClient } from "@dfinity/auth-client";
 
 async function run() {
   const authClient = await AuthClient.create();
-  await authClient.login({
-    identityProvider: "https://identity.ic0.app/",
-    onSuccess: async () => {
-      const identity = authClient.getIdentity();
-      const principal = identity.getPrincipal().toText();
-      // Redirect to your app with the principal
-      window.location.href = `icpapp://login?principal=${principal}`;
-    }
-  });
+  if (await authClient.isAuthenticated()) {
+    const identity = authClient.getIdentity();
+    const principal = identity.getPrincipal().toText();
+    window.location.href = `icpapp://login?principal=${principal}`;
+  } else {
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/",
+      onSuccess: async () => {
+        const identity = authClient.getIdentity();
+        const principal = identity.getPrincipal().toText();
+        window.location.href = `icpapp://login?principal=${principal}`;
+      }
+    });
+  }
 }
 
 run(); 
